548622fb45d6315bbc06585ff33e20c0a9e06cd6
abhisheksat <abhisheksatpute11@gmail.com>
2020-05-08 22:25:25 -0400

    update to dish service

--- 26. Saving changes to server/conFusion/conFusion/src/app/services/dish.service.ts
+++ 26. Saving changes to server/conFusion/conFusion/src/app/services/dish.service.ts
@@ -32,7 +32,7 @@ export class DishService {
 
   getFeaturedDish(): Observable<Dish> {
     // return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));
-    return this.http.get<Dish[]>(baseURL + 'dishees?featured=true')
+    return this.http.get<Dish[]>(baseURL + 'dishes?featured=true')
       .pipe(map(dishes => dishes[0]))
       .pipe(catchError(this.processHTTPMsgService.handleError));
   }
