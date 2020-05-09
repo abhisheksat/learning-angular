543c06708f067b1b3f979e60e63cb4ef9a96a966
abhisheksat <abhisheksatpute11@gmail.com>
2020-05-08 23:04:19 -0400

    Angular Animations - Part 1

---
+++ 28. Angular Animations - Part 1/conFusion/conFusion/src/app/home/home.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { Promotion } from '../shared/promotion';
+import { PromotionService } from '../services/promotion.service';
+
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  dish: Dish;
+  dishErrMess: string;
+  promotion: Promotion;
+  leader: Leader;
+
+  constructor(private dishService: DishService,
+    private promotionService: PromotionService,
+    private leaderService: LeaderService,
+    @Inject('BaseURL') private BaseURL) { }
+
+  ngOnInit() {
+    this.dishService.getFeaturedDish()
+      .subscribe((featuredDish) => this.dish = featuredDish,
+        errmess => this.dishErrMess = <any> errmess);
+
+    this.promotionService.getFeaturedPromotion()
+      .subscribe((featuredPromotion) => this.promotion = featuredPromotion);
+    
+    this.leaderService.getFeaturedLeader()
+      .subscribe((leader) => this.leader = leader);
+  }
+
+}
